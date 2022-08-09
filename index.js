d08e79f91c44018158fd210a0db01226a0afa891
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-09 11:43:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = 'Timothy is the champion';
